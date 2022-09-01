9c50e8951e9f9b3798b651c58a50dcd8c8f1f700
java110 <928255095@qq.com>
2022-09-01 14:00:22 +0800

    优化代码

--- back/iot-api/src/main/java/com/java110/api/aop/ExtApiAop.java
+++ back/iot-api/src/main/java/com/java110/api/aop/ExtApiAop.java
@@ -33,7 +33,7 @@ public class ExtApiAop {
 
     public static final String ACCESS_TOKEN = "access_token";
 
-    @Pointcut("execution(public * com.java110.things.extApi..*.*Controller.*(..))")
+    @Pointcut("execution(public * com.java110.api.extApi..*.*Controller.*(..))")
     public void dataProcess() {
     }
 
